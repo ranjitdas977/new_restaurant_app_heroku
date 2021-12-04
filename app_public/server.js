981c22af8bc72468bde45db1f0faa0e656719f76
Ranjit Das <ranjitdas977@gmail.com>
2021-12-03 19:16:36 -0800

    few changes

--- app_public/server.js
+++ app_public/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/NEW_RESTAURANT_APP'));
+app.use(express.static('./dist/NEW_RESTAURANT_APP_HEROKU'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
