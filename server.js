01383f1bd7454ace1d7f0e75507d95dec3a2013e
Ranjit Das <ranjitdas977@gmail.com>
2021-12-04 12:05:41 -0800

    make it better

--- server.js
+++ server.js
@@ -5,12 +5,16 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/NEW_RESTAURANT_APP'));
+app.use(express.static('./dist/new_restaurant_app_heroku'));
 
 
 app.get('*', (req, res) => {
     res.sendFile(`./front-end/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
 });
+///
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/new_restaurant_app_heroku/'}),
+);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
